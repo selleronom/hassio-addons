6d63733a87666177d4579c2f4324a132c7f00bf1
selleronom <3093210+selleronom@users.noreply.github.com>
2019-11-03 16:20:22 +0100

    mkfifo

--- raspotify/run.sh
+++ raspotify/run.sh
@@ -5,6 +5,7 @@ CONFIG_PATH=/data/options.json
 LIBRESPOT_OPTS=$(jq --raw-output ".librespotopts" $CONFIG_PATH)
 
 mkdir -p /share/snapfifo
+mkfifo /share/snapfifo/librespot
 
 echo "Start Librespot..."
 librespot ${LIBRESPOT_OPTS}
