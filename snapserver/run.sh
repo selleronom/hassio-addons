cf198367ba15e8047188ac17abdea26f8b49a40c
selleronom <3093210+selleronom@users.noreply.github.com>
2019-11-03 16:58:47 +0100

    mkdir

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -5,6 +5,7 @@ CONFIG_PATH=/data/options.json
 SNAPSERVER_OPTS=$(jq --raw-output ".snapserveropts" $CONFIG_PATH)
 
 mkdir -p /share/snapfifo
+mkdir -p /var/lib/snapserver/
 printf '{\n\t"ConfigVersion": 2,\n\t"Groups": []\n}' > /var/lib/snapserver/server.json
 
 echo "Start Snapserver..."
