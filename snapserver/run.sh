806720033ecf95c41625411e13d5a9beb80021a1
Erik <3093210+selleronom@users.noreply.github.com>
2019-12-15 19:25:24 +0100

    2nd cleanup

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-#echo "Start DBUS-daemon..."
-#/usr/bin/dbus-daemon --system
-#echo "Started"
-
-echo "Start Avahi-daemon..."
-/usr/sbin/avahi-daemon --no-drop-root
-echo "Started"
-
 CONFIG_PATH=/data/options.json
 
 SNAPSERVER_OPTS=$(jq --raw-output ".snapserveropts" $CONFIG_PATH)
 
 mkdir -p /share/snapfifo
 mkdir -p /var/lib/snapserver/
-printf '{\n\t"ConfigVersion": 2,\n\t"Groups": []\n}' > /var/lib/snapserver/server.json
+printf '{\n\t"ConfigVersion": 2,\n\t"Groups": []\n}' > /root/.config/snapserver/server.json
 
 echo "Start Snapserver..."
 /usr/bin/snapserver ${SNAPSERVER_OPTS}
