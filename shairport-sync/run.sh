b5b71e39751a19ca07ea61781ca783627236347e
Erik <3093210+selleronom@users.noreply.github.com>
2019-12-15 20:14:51 +0100

    Update run.sh

--- shairport-sync/run.sh
+++ shairport-sync/run.sh
@@ -4,8 +4,8 @@ mkdir -p /share/snapfifo
 
 CONFIG_PATH=/data/options.json
 
-SHAIRPORT_OPTS=$(jq --raw-output ".name" $CONFIG_PATH)
+SHAIRPORT_NAME=$(jq --raw-output ".name" $CONFIG_PATH)
 
 echo "Start Shairport-sync..."
-/usr/bin/shairport-sync -a {$SHAIRPORT_OPTS}
+/usr/bin/shairport-sync -a $SHAIRPORT_NAME
 echo "Started"
