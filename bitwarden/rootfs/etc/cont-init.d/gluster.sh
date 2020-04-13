2f9c876acb148b5901f007ed0db19729aaccd5fe
Selleronom <3093210+selleronom@users.noreply.github.com>
2020-04-13 17:39:27 +0200

    add bitwarden mounted to gluster

---
+++ bitwarden/rootfs/etc/cont-init.d/gluster.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Bitwarden
+# ==============================================================================
+
+exec /bin/mount -t glusterfs localhost:/gv0 /data
