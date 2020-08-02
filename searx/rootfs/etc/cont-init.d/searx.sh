d8b8b48a8f438dc32f5954bffcbbec0af49d2a04
Selleronom <3093210+selleronom@users.noreply.github.com>
2020-08-02 18:01:38 +0200

    added option for image_proxy

--- searx/rootfs/etc/cont-init.d/searx.sh
+++ searx/rootfs/etc/cont-init.d/searx.sh
@@ -9,6 +9,7 @@ sed -i 's/autocomplete : ""/autocomplete : '"$(bashio::config 'autocomplete')"'/
 sed -i 's/secret_key : "ultrasecretkey"/secret_key : '"$(bashio::config 'secret_key')"'/g' "${config}"
 sed -i 's~base_url : False~base_url : '"$(bashio::config 'base_url')"'~g' "${config}"
 sed -i 's/http_protocol_version : "1.0"/http_protocol_version : '"$(bashio::config 'http_protocol_version')"'/g' "${config}"
+sed -i 's/image_proxy : "False"/image_proxy : '"$(bashio::config 'image_proxy')"'/g' "${config}"
 
 if bashio::var.true $(bashio::config 'enable_all_engines'); then
     sed -i '/disabled : True/d' "${config}"
