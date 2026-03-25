3b3ee9dfcc7668d7832c32c568748127069a6d35
selleronom <3093210+selleronom@users.noreply.github.com>
2026-03-25 12:19:15 +0100

    ⬆️ Update Immich version to 2.6.2-7, enhance process cleanup and port management in service scripts
    Signed-off-by: selleronom <3093210+selleronom@users.noreply.github.com>

--- immich/rootfs/usr/local/bin/immich-server-wrapper.sh
+++ immich/rootfs/usr/local/bin/immich-server-wrapper.sh
@@ -6,6 +6,7 @@ export IMMICH_ENV=production
 export IMMICH_PORT="${SERVER_PORT:-2283}"
 export IMMICH_HOST=0.0.0.0
 export HOME="/data"
+export NODE_OPTIONS="--dns-result-order=ipv4first"
 
 if [[ -x /usr/src/app/server/bin/start.sh ]]; then
 	exec /bin/bash -c 'cd /usr/src/app/server/bin && exec ./start.sh'
